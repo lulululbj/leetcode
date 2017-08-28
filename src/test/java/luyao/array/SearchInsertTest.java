fa95edf087fdcbf9074a8e5aa31d62ea9ae130a6
luyao <sunluyao1993x@gmail.com>
2017-08-28 11:19:38 +0800

    SearchInsert

---
+++ src/test/java/luyao/array/SearchInsertTest.java
@@ -0,0 +1,27 @@
+package luyao.array;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by luyao
+ * on 2017/8/28 10:47
+ */
+public class SearchInsertTest {
+    @Test
+    public void solution() throws Exception {
+
+        int[] nums = {1, 3, 5, 6};
+
+        assertEquals(SearchInsert.solution(nums,5),2);
+        assertEquals(SearchInsert.solution(nums,2),1);
+        assertEquals(SearchInsert.solution(nums,3),1);
+        assertEquals(SearchInsert.solution(nums,0),0);
+        assertEquals(SearchInsert.solution(nums,7),4);
+
+        int[] nums2 = {1, 3};
+        assertEquals(1,SearchInsert.solution(nums2,2));
+    }
+
+}
