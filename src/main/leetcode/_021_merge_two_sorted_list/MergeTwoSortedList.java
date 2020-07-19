9f8042d8d43c594bc11b4e2807a852e7e45fe0bf
luyao <sunluyao1993x@gmail.com>
2020-07-19 19:44:41 +0800

    21. 合并两个有序链表

--- src/main/leetcode/_021_merge_two_sorted_list/MergeTwoSortedList.java
+++ src/main/leetcode/_021_merge_two_sorted_list/MergeTwoSortedList.java
@@ -1,41 +1,42 @@
 package leetcode._021_merge_two_sorted_list;
 
-//Merge two sorted linked lists and return it as a new list. The new list should be made by splicing together the nodes of the first two lists.
-//
-//        Example:
-//
-//        Input: 1->2->4, 1->3->4
-//        Output: 1->1->2->3->4->4
+/*
+ * 将两个升序链表合并为一个新的 升序 链表并返回。新链表是通过拼接给定的两个链表的所有节点组成的。 
+ *
+ * 示例：
+ *
+ * 输入：1->2->4, 1->3->4
+ * 输出：1->1->2->3->4->4
+ *
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/merge-two-sorted-lists
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
 
 import leetcode.data.ListNode;
 
 public class MergeTwoSortedList {
 
 
-    /**
+    /*
+     * 每个链表一个指针，依次往前挪，并比较大小
      * Time Complexity: O(n)
      * Space Complexity: O(1)
      */
     public ListNode solution(ListNode l1, ListNode l2) {
-
-        ListNode head = new ListNode(0);
-        ListNode node = head;
-        while (l1 != null && l2 != null) {
-            if (l1.val > l2.val) {
-                node.next = l2;
-                l2 = l2.next;
-
-            } else {
+        ListNode dummy = new ListNode(0);
+        ListNode node = dummy;
+        while (l1 != null && l2 != null){
+            if (l1.val < l2.val){
                 node.next = l1;
                 l1 = l1.next;
+            }else {
+                node.next = l2;
+                l2 = l2.next;
             }
             node = node.next;
-
         }
-
-        if (l1!=null) node.next=l1;
-        if (l2!=null) node.next=l2;
-
-        return head.next;
+        node.next = l1 == null ? l2 : l1;
+        return dummy.next;
     }
 }
